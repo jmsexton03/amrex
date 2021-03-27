b52d72c351e3e8cb9023f685710d489d8b988776
Jean M. Sexton <jmsexton@lbl.gov>
2021-03-26 19:42:03 -0500

    Fix indent

--- Tests/GPU/BaseFab/main.cpp
+++ Tests/GPU/BaseFab/main.cpp
@@ -23,28 +23,28 @@ int main (int argc, char* argv[])
       {
         // ParmParse is way of reading inputs from the inputs file
         ParmParse pp;
-
+        
         // We need to get n_cell from the inputs file - this is the number of cells on each side of
         //   a square (or cubic) domain.
         pp.query("n_cell",n_cell);
 
       }
-        IntVect dom_lo(AMREX_D_DECL(       0,        0,        0));
-        IntVect dom_hi(AMREX_D_DECL(n_cell-1, n_cell-1, n_cell-1));
-        Box domain(dom_lo, dom_hi);
-        BaseFab<int> localtouch(The_Cpu_Arena()), remotetouch(The_Cpu_Arena());
-	bool check_local, check_remote, m_threadsafe_loc, m_threadsafe_rcv;
-	localtouch.resize(domain);
-	localtouch.setVal<RunOn::Host>(0);
-	remotetouch.resize(domain);
-	remotetouch.setVal<RunOn::Host>(2);
-	BL_PROFILE_VAR("CPU-max-1",max1);
-	check_local = m_threadsafe_loc = localtouch.max<RunOn::Host>() <= 1;
-	BL_PROFILE_VAR_STOP(max1);
-	  BL_PROFILE_VAR("CPU-max-2",max2);
-	check_remote = m_threadsafe_rcv = remotetouch.max<RunOn::Host>() <= 1;
-	BL_PROFILE_VAR_STOP(max2);
-	amrex::Print()<<"Found these max values: "<<check_local<<"\t"<<check_remote<<std::endl;
+      IntVect dom_lo(AMREX_D_DECL(       0,        0,        0));
+      IntVect dom_hi(AMREX_D_DECL(n_cell-1, n_cell-1, n_cell-1));
+      Box domain(dom_lo, dom_hi);
+      BaseFab<int> localtouch(The_Cpu_Arena()), remotetouch(The_Cpu_Arena());
+      bool check_local, check_remote, m_threadsafe_loc, m_threadsafe_rcv;
+      localtouch.resize(domain);
+      localtouch.setVal<RunOn::Host>(0);
+      remotetouch.resize(domain);
+      remotetouch.setVal<RunOn::Host>(2);
+      BL_PROFILE_VAR("CPU-max-1",max1);
+      check_local = m_threadsafe_loc = localtouch.max<RunOn::Host>() <= 1;
+      BL_PROFILE_VAR_STOP(max1);
+      BL_PROFILE_VAR("CPU-max-2",max2);
+      check_remote = m_threadsafe_rcv = remotetouch.max<RunOn::Host>() <= 1;
+      BL_PROFILE_VAR_STOP(max2);
+      amrex::Print()<<"Found these max values: "<<check_local<<"\t"<<check_remote<<std::endl;
     }
     amrex::Finalize();
 }
